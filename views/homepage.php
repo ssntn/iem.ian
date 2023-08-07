0c1f9834bf5cbb2a96d5aa86b51aa7fce2549471
ssntn <iansisante@gmail.com>
2023-08-07 21:07:43 +0800

    iems

--- views/homepage.php
+++ views/homepage.php
@@ -7,13 +7,37 @@
     </div>
 </section>
 
+<section class="d-flex flex-column align-items-center ">
+    <h3 class="pb-3">My IEM experience</h2>
+    <div class="row pb-2">
+        <div class="card col-sm m-2" style="width: 18rem;">
+            <img src="https://dtt1c9id3txwq.cloudfront.net/assets/images/001/386/813/original/kz-edx-ultra-img01.jpg?1654011241" class="card-img-top" alt="KZ EDX Pro">
+            <div class="card-body">
+                <h5 class="card-title">KZ EDX pro</h5>
+                <p class="card-text">The highs are too high, but very good bass. Not recommended for games with breaking glasses.</p>
+            </div>
+        </div>
+        
+        <div class="card row-sm m-2" style="width: 18rem;">
+            <img src="https://makelifeclick.com/wp-content/uploads/2023/03/KZ-EDX-IEM-1024x683.jpg" class="card-img-top" alt="KZ EDX">
+            <div class="card-body">
+                <h5 class="card-title">KZ EDX pro</h5>
+                <p class="card-text">The very nice mids and high compared to EDX Pro, and a really  good bass. </p>
+            </div>
+        </div>
+        
+        <div class="card row-sm m-2" style="width: 18rem;">
+            <img src="https://i.ebayimg.com/images/g/qZQAAOSwS3VfGpgW/s-l1200.jpg" class="card-img-top" alt="KZ EDX">
+            <div class="card-body">
+                <h5 class="card-title">KZ EDX pro</h5>
+                <p class="card-text">Best overall. A bit pricey but really good for everything. A bit on the bigger side though.</p>
+            </div>
+        </div>
 
-<section>
-    <h3>My IEM experience</h2>
-    <div>
-        <p>List</p>
+
     </div>
-</section>
     
+</section>
+
     
 <?php include './components/footer.php' ?>
